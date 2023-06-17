b348ba0b0a0a1a38f4d4c2a890df2d57d2ae62f6
www.byteblogs.com <598092184@qq.com>
2023-06-17 18:55:55 +0800

    feat: 2.0.0 1. 添加启动logo

--- easy-retry-client-core/src/main/java/com/aizuda/easy/retry/client/core/init/EasyRetryStartListener.java
+++ easy-retry-client-core/src/main/java/com/aizuda/easy/retry/client/core/init/EasyRetryStartListener.java
@@ -1,11 +1,15 @@
 package com.aizuda.easy.retry.client.core.init;
 
 import com.aizuda.easy.retry.client.core.Lifecycle;
+import com.aizuda.easy.retry.common.core.constant.SystemConstants;
 import com.aizuda.easy.retry.common.core.util.EasyRetryVersion;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.helpers.MessageFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -25,8 +29,11 @@ public class EasyRetryStartListener implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(MessageFormatter.format(SystemConstants.LOGO, EasyRetryVersion.getVersion()).getMessage());
+
         log.info("Easy-Retry client is preparing to start... v{}", EasyRetryVersion.getVersion());
         lifecycleList.forEach(Lifecycle::start);
         log.info("Easy-Retry client started successfully v{}", EasyRetryVersion.getVersion());
     }
+
 }
