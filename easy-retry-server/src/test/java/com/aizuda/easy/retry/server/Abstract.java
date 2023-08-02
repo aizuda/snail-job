50f4435c255eead8f4379c26f9c062ca71638e11
高卫强 <gao.gwq@alibaba-inc.com>
2023-08-02 13:50:00 +0800

    feat:增加测试抽象父类

---
+++ easy-retry-server/src/test/java/com/aizuda/easy/retry/server/Abstract.java
@@ -0,0 +1,10 @@
+package com.aizuda.easy.retry.server;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EasyRetryServerApplication.class)
+public class Abstract {
+}
