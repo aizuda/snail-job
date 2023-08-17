64e962bc8fb1b11f7f84706c50e3b165ac7c61af
byteblogs168 <598092184@qq.com>
2023-08-17 15:52:24 +0800

    feat: 2.1.1 1. 修复windows操作系统启动失败问题

--- easy-retry-server/src/main/java/com/aizuda/easy/retry/server/EasyRetryServerApplication.java
+++ easy-retry-server/src/main/java/com/aizuda/easy/retry/server/EasyRetryServerApplication.java
@@ -1,6 +1,7 @@
 package com.aizuda.easy.retry.server;
 
 import com.aizuda.easy.retry.server.server.NettyHttpServer;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,10 +15,12 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @MapperScan("com.aizuda.easy.retry.server.persistence.mybatis.mapper")
 @EnableTransactionManagement(proxyTargetClass = true)
+@Slf4j
 public class EasyRetryServerApplication {
 
     @Bean
@@ -36,7 +39,16 @@ public class EasyRetryServerApplication {
     @Bean
     public ApplicationRunner nettyStartupChecker(NettyHttpServer nettyHttpServer, ServletWebServerFactory serverFactory) {
         return args -> {
+            // 最长自旋10秒，保证nettyHttpServer启动完成
+            int waitCount = 0;
+            while (!nettyHttpServer.isStarted() || waitCount > 100) {
+                log.info("--------> easy-retry netty server is staring....");
+                TimeUnit.MILLISECONDS.sleep(100);
+                waitCount++;
+            }
+
             if (!nettyHttpServer.isStarted()) {
+                log.error("--------> easy-retry netty server startup failure.");
                 // Netty启动失败，停止Web服务和Spring Boot应用程序
                 serverFactory.getWebServer().stop();
                 SpringApplication.exit(SpringApplication.run(EasyRetryServerApplication.class));
