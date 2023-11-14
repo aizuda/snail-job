f2b1f015886570b4ca969c62efd435706ca1b7c5
byteblogs168 <598092184@qq.com>
2023-11-14 23:28:03 +0800

    feat:2.4.0 1. 修改文案

--- frontend/src/locales/lang/zh-CN.js
+++ frontend/src/locales/lang/zh-CN.js
@@ -21,7 +21,7 @@ export default {
 
   'layouts.usermenu.dialog.title': '信息',
   'layouts.usermenu.dialog.content': '您确定要注销吗？',
-  'layouts.userLayout.title': '致力提高分布式业务系统一致性的分布式重试平台',
+  'layouts.userLayout.title': '一款为了提高分布式业务系统一致性的分布式任务重试和分布式任务调度平台',
   ...components,
   ...global,
   ...menu,
