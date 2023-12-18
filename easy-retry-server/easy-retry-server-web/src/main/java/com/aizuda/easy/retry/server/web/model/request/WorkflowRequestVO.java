f0995c33973396256706c22cc9a4f3685be6bb1b
byteblogs168 <598092184@qq.com>
2023-12-19 00:03:12 +0800

    feat: 2.6.0 1. 优化查询DAG详情

--- easy-retry-server/easy-retry-server-web/src/main/java/com/aizuda/easy/retry/server/web/model/request/WorkflowRequestVO.java
+++ easy-retry-server/easy-retry-server-web/src/main/java/com/aizuda/easy/retry/server/web/model/request/WorkflowRequestVO.java
@@ -17,6 +17,8 @@ import java.util.List;
 @Data
 public class WorkflowRequestVO {
 
+    private Long id;
+
     @NotBlank(message = "组名称不能为空")
     @Pattern(regexp = "^[A-Za-z0-9_]{1,64}$", message = "仅支持长度为1~64字符且类型为数字、字母和下划线")
     private String groupName;
