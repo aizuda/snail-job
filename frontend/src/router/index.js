ecbb953bbb52ae7e86366b554ef072e916ef0a68
xlsea <m@xlsea.cn>
2023-12-19 16:06:30 +0800

    feat: 2.6.0 优化CRON页面

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,7 @@ Router.prototype.push = function push (location, onResolve, onReject) {
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  base: '/easy-retry/',
+  mode: 'history',
   routes: constantRouterMap
 })
