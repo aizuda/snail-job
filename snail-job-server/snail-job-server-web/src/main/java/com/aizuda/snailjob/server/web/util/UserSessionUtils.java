8322d510fe3669493d4eb093c31c8d7d2302247c
dhb52 <dhb52@126.com>
2024-05-05 21:59:34 +0800

    refactor: 简化JobBatchServiceImpl.getJobBatchPage代码，抽取UserSessionUtils.hasGroupPermission函数

--- snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/util/UserSessionUtils.java
+++ snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/util/UserSessionUtils.java
@@ -1,11 +1,11 @@
 package com.aizuda.snailjob.server.web.util;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.aizuda.snailjob.server.web.model.request.UserSessionVO;
-import com.aizuda.snailjob.server.web.model.request.UserSessionVO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * @author opensnail
@@ -17,5 +17,24 @@ public final class UserSessionUtils {
     public static UserSessionVO currentUserSession() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         return (UserSessionVO) request.getAttribute("currentUser");
+    }
+
+    /**
+     * 判断当前用户是否具备组权限
+     *
+     * @param groupName 组名称
+     * @return true, 具备权限，否则返回false
+     */
+    public static boolean hasGroupPermission(String groupName) {
+        if (StrUtil.isBlank(groupName)) {
+            return true;
+        }
+
+        UserSessionVO userSessionVO = currentUserSession();
+        if (userSessionVO.isUser() && CollUtil.isNotEmpty(userSessionVO.getGroupNames())) {
+            return userSessionVO.getGroupNames().contains(groupName);
+        }
+
+        return false;
     }
 }
