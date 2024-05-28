d930624185268b84f429928c14f674302bb63bf5
dhb52 <dhb52@126.com>
2024-05-28 08:55:01 +0800

    refactor(sj_1.0.0): 使用@Valid+@Validated 校验 Service 数据传递

--- snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/util/ImportUtils.java
+++ snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/util/ImportUtils.java
@@ -1,21 +1,14 @@
 package com.aizuda.snailjob.server.web.util;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.Assert;
 import com.aizuda.snailjob.common.core.exception.SnailJobCommonException;
 import com.aizuda.snailjob.common.core.util.JsonUtil;
-import com.aizuda.snailjob.server.common.exception.SnailJobServerException;
 import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 public class ImportUtils {
 
@@ -30,17 +23,6 @@ public class ImportUtils {
 
         JsonNode node = JsonUtil.toJson(file.getBytes());
         List<VO> requestList = JsonUtil.parseList(JsonUtil.toJsonString(node), clazz);
-        Assert.notEmpty(requestList, () -> new SnailJobServerException("导入数据不能为空"));
-
-        // 校验参数是否合法
-        requestList.forEach(vo -> {
-            ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
-            Validator validator = vf.getValidator();
-            Set<ConstraintViolation<VO>> rules = validator.validate(vo);
-            rules.forEach(rule -> {
-                throw new SnailJobCommonException(rule.getMessage());
-            });
-        });
 
         return requestList;
     }
