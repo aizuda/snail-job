77dfd581820633ac68b538e19ef603678379313d
opensnail <598092184@qq.com>
2024-06-03 22:50:52 +0800

    feat(sj_1.0.0): 修复sqlserver脚本执行失败问题

--- doc/docker/sqlserver/create_schema.sh
+++ doc/docker/sqlserver/create_schema.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'SnailJob@24' -Q "CREATE DATABASE snail_job;
 GO"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'SnailJob@24' -d snail_job -i /tmp/schema.sql
