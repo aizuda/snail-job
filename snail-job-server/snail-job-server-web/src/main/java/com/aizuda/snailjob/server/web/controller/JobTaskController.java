5391fe1d184766c0243d5b64e86b396121d7bd1a
opensnail <598092184@qq.com>
2024-06-20 18:37:50 +0800

    fix(sj_1.1.0): 新增任务项列表查询接口 1. 任务项列表以树形方式展示任务列表

--- snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/controller/JobTaskController.java
+++ snail-job-server/snail-job-server-web/src/main/java/com/aizuda/snailjob/server/web/controller/JobTaskController.java
@@ -1,11 +1,12 @@
 package com.aizuda.snailjob.server.web.controller;
 
+import cn.hutool.core.lang.tree.Tree;
 import com.aizuda.snailjob.server.web.annotation.LoginRequired;
 import com.aizuda.snailjob.server.web.model.base.PageResult;
 import com.aizuda.snailjob.server.web.model.request.JobTaskQueryVO;
 import com.aizuda.snailjob.server.web.model.response.JobTaskResponseVO;
 import com.aizuda.snailjob.server.web.service.JobTaskService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +20,20 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/job/task")
+@RequiredArgsConstructor
 public class JobTaskController {
-
-    @Autowired
-    private JobTaskService jobTaskService;
+    private final JobTaskService jobTaskService;
 
     @GetMapping("/list")
     @LoginRequired
     public PageResult<List<JobTaskResponseVO>> getJobTaskPage(JobTaskQueryVO jobTaskQueryVO) {
         return jobTaskService.getJobTaskPage(jobTaskQueryVO);
+    }
+
+    @GetMapping("/tree/list")
+    @LoginRequired
+    public List<Tree<Long>> getTreeJobTask(JobTaskQueryVO jobTaskQueryVO) {
+        return jobTaskService.getTreeJobTask(jobTaskQueryVO);
     }
 
 }
