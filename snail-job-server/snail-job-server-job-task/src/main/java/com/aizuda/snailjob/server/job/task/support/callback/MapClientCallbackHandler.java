ac4fbf60f641b001b72dc421d273e8caf16b8f6f
opensnail <598092184@qq.com>
2024-06-24 23:58:34 +0800

    feat(sj_1.1.0-beta2): 优化获取ip工具

---
+++ snail-job-server/snail-job-server-job-task/src/main/java/com/aizuda/snailjob/server/job/task/support/callback/MapClientCallbackHandler.java
@@ -0,0 +1,27 @@
+package com.aizuda.snailjob.server.job.task.support.callback;
+
+import com.aizuda.snailjob.common.core.enums.JobTaskTypeEnum;
+import com.aizuda.snailjob.template.datasource.persistence.mapper.JobTaskMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author opensnail
+ * @date 2024-06-24 22:51:20
+ * @since sj_1.1.0
+ */
+@Component
+public class MapClientCallbackHandler extends MapReduceClientCallbackHandler {
+    public MapClientCallbackHandler(JobTaskMapper jobTaskMapper) {
+        super(jobTaskMapper);
+    }
+
+    @Override
+    public JobTaskTypeEnum getTaskInstanceType() {
+        return JobTaskTypeEnum.MAP;
+    }
+
+    @Override
+    public void callback(ClientCallbackContext context) {
+        super.callback(context);
+    }
+}
