ba9a2d69447b9af5014b29ba1fc378b8ed618f8b
dhb52 <dhb52@126.com>
2024-07-10 23:18:22 +0800

    feat(sj_1.1.0-beta3): 新增执行器类型枚举ExecutorTypeEnum

---
+++ snail-job-common/snail-job-common-core/src/main/java/com/aizuda/snailjob/common/core/enums/ExecutorTypeEnum.java
@@ -0,0 +1,23 @@
+package com.aizuda.snailjob.common.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 执行器类型枚举
+ *
+ * @author dhb52
+ * @date 2024-07-09 23:48:55
+ * @since 1.1.0
+ */
+@AllArgsConstructor
+@Getter
+public enum ExecutorTypeEnum {
+
+    JAVA(1),
+    PYTHON(2),
+    ;
+
+    private final int type;
+
+}
