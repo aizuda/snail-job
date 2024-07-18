ef7c655640ebba950c6ab17c54f191f73fbb4114
opensnail <598092184@qq.com>
2024-07-18 09:44:20 +0800

    fix(sj_1.1.1): 发布1.1.1

--- snail-job-server/snail-job-server-ui/src/main/resources/admin/assets/index-J-FakGXJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./exception-base.vue_vue_type_script_setup_true_lang-SB5pfeDj.js";import{d as n,o as t,c as a}from"./index-BxI_8ir5.js";const m=n({name:"404",__name:"index",setup(c){return(_,s)=>{const e=o;return t(),a(e,{type:"404"})}}});export{m as default};
