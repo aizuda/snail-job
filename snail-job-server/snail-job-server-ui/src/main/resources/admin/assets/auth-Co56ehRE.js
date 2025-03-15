bd2ea29b51e54b84e1e3c139773c51f53f9cbe89
opensnail <598092184@qq.com>
2025-03-15 10:09:55 +0800

    feat(1.4.0-beta2): 发布1.4.0-beta2

--- snail-job-server/snail-job-server-ui/src/main/resources/admin/assets/auth-Co56ehRE.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-BHXExu3l.js";function o(){const r=n();function u(t){return r.isLogin?typeof t=="string"?r.userInfo.roles.includes(t):t.some(e=>r.userInfo.roles.includes(e)):!1}return{hasAuth:u}}export{o as u};
