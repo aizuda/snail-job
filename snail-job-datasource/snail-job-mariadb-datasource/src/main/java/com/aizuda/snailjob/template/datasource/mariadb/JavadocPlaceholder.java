6d119abea26f32270de582c8f297b28c6921acfe
opensnail <598092184@qq.com>
2025-06-22 11:25:30 +0800

    feat(1.6.0-beta1): 解决推送maven仓库失败问题

---
+++ snail-job-datasource/snail-job-mariadb-datasource/src/main/java/com/aizuda/snailjob/template/datasource/mariadb/JavadocPlaceholder.java
@@ -0,0 +1,12 @@
+package com.aizuda.snailjob.template.datasource.mariadb;
+
+/**
+ * <p>
+ * Placeholder class to ensure javadoc.jar is not empty.
+ * </p>
+ *
+ * @author opensnail
+ * @date 2025-06-22
+ */
+public class JavadocPlaceholder {
+}
---
+++ snail-job-datasource/snail-job-oracle-datasource/src/main/java/com/aizuda/snailjob/template/datasource/oracle/JavadocPlaceholder.java
@@ -0,0 +1,12 @@
+package com.aizuda.snailjob.template.datasource.oracle;
+
+/**
+ * <p>
+ * Placeholder class to ensure javadoc.jar is not empty.
+ * </p>
+ * 
+ * @author opensnail
+ * @date 2025-06-22
+ */
+public class JavadocPlaceholder {
+}
