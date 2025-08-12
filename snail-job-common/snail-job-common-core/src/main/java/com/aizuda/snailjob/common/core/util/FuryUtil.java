eb24bc9e44b7d0a95a2d97ccf540b9eb7718542f
srzou <srzou@outlook.com>
2025-08-12 15:10:44 +0800

    feat(1.7.2): 1、fury升级fory; 2、修复(A Java deserialization vulnerability in SnailJob)

--- snail-job-common/snail-job-common-core/src/main/java/com/aizuda/snailjob/common/core/util/FuryUtil.java
+++ snail-job-common/snail-job-common-core/src/main/java/com/aizuda/snailjob/common/core/util/FuryUtil.java
@@ -5,13 +5,20 @@ import com.aizuda.snailjob.common.core.context.SnailSpringContext;
 import com.aizuda.snailjob.common.core.exception.SnailJobCommonException;
 import com.aizuda.snailjob.common.log.SnailJobLog;
 import com.github.luben.zstd.Zstd;
-import org.apache.fury.Fury;
-import org.apache.fury.ThreadSafeFury;
-import org.apache.fury.config.CompatibleMode;
-import org.apache.fury.config.Language;
+import org.apache.fory.Fory;
+import org.apache.fory.ThreadSafeFory;
+import org.apache.fory.config.CompatibleMode;
+import org.apache.fory.config.Language;
+import org.apache.fory.resolver.AllowListChecker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Base64;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class FuryUtil {
@@ -20,16 +27,55 @@ public class FuryUtil {
      */
     private static final int DEFAULT_MAX_DECOMPRESSED_SIZE = 16384;
 
-    private static final ThreadSafeFury SERIALIZER = Fury.builder()
-            .withLanguage(Language.JAVA)
-            .requireClassRegistration(false)
-            .withCompatibleMode(CompatibleMode.COMPATIBLE)
-            .buildThreadSafeFuryPool(
-                    Runtime.getRuntime().availableProcessors(),
-                    Runtime.getRuntime().availableProcessors() * 2,
-                    30,
-                    TimeUnit.MINUTES
-            );
+    private static final String FORY_BLACK_LIST = "foryBlackList.txt";
+
+    private static final ThreadSafeFory SERIALIZER;
+
+    static {
+        // fix => https://gitee.com/aizuda/snail-job/issues/ICQV61
+        AllowListChecker checker = new AllowListChecker(AllowListChecker.CheckLevel.STRICT);
+
+        ThreadSafeFory fory = Fory.builder()
+                .withLanguage(Language.JAVA)
+                .requireClassRegistration(true)
+                .withCompatibleMode(CompatibleMode.COMPATIBLE)
+                .buildThreadSafeForyPool(
+                        Runtime.getRuntime().availableProcessors(),
+                        Runtime.getRuntime().availableProcessors() * 2,
+                        30,
+                        TimeUnit.MINUTES
+                );
+        fory.setClassChecker(checker);
+
+        Set<String> disableClasses = getDisallowClasses();
+        checker.disallowClasses(disableClasses);
+
+        SERIALIZER = fory;
+    }
+
+    private static final Set<String> getDisallowClasses (){
+        // 读取黑名单配置
+        Set<String> disallowClasses = new HashSet<>();
+        // 使用类加载器读取resources目录下的配置文件
+        try (InputStream is = FuryUtil.class.getClassLoader().getResourceAsStream(FORY_BLACK_LIST);
+             BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(is)))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                // 跳过空行和注释行（假设以#开头的是注释）
+                if (!line.isEmpty() && !line.startsWith("#")) {
+                    disallowClasses.add(line);
+                }
+            }
+        } catch (NullPointerException e) {
+            SnailJobLog.LOCAL.error("foryBlackList.txt file not found in resources", e);
+        } catch (IOException e) {
+            SnailJobLog.LOCAL.error("Failed to read foryBlackList.txt", e);
+        }
+        return disallowClasses;
+    }
+
 
     public static String serialize(Object object) {
         if (object == null) {
@@ -60,7 +106,7 @@ public class FuryUtil {
         if (size > decompressedSize){
             throw new SnailJobCommonException("Decompressed size exceeds the allowed limit.");
         }
-        bytes = Zstd.decompress(bytes, (int) Zstd.decompressedSize(bytes));
+        bytes = Zstd.decompress(bytes, size);
         //noinspection unchecked
         return (T) SERIALIZER.deserialize(bytes);
     }
